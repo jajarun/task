82ed0bd6604ac181b86a6a4621f5638b6364a55c
majh <majiahao@yiwei.com>
2023-03-24 18:07:05 +0800

    debug

--- ws/commet.go
+++ ws/commet.go
@@ -14,6 +14,17 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+var userCons = make(map[string]map[*websocket.Conn]bool)
+
+func handleMessage(msg string, userId string) {
+	for conn, _ := range userCons[userId] {
+		err := conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("收到%s的消息%s", userId, msg)))
+		if err != nil {
+			log.Println("write msg err:", err)
+		}
+	}
+}
+
 func Run() {
 
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
@@ -32,29 +43,41 @@ func Run() {
 			return
 		}
 		//defer conn.Close()
-		query := request.URL.RawQuery
-		_ = conn.WriteMessage(websocket.TextMessage, []byte(query+"连接成功"))
+		query := request.URL.Query()
+		userId := query.Get("user_id")
+		if userId == "" {
+			log.Println("user_id 为空 关闭连接")
+			_ = conn.Close()
+			return
+		}
+		if userCons[userId] == nil {
+			userCons[userId] = make(map[*websocket.Conn]bool)
+		}
+		userCons[userId][conn] = true
+		_ = conn.WriteMessage(websocket.TextMessage, []byte(userId+"连接成功"))
+		log.Println(userId + "连接数" + strconv.Itoa(len(userCons[userId])))
 		conn.SetCloseHandler(func(code int, text string) error {
-			log.Println(query + "关闭连接")
+			delete(userCons[userId], conn)
+			log.Println(userId + "关闭连接")
+			log.Println(userId + "连接数" + strconv.Itoa(len(userCons[userId])))
 			return nil
 		})
-		go func(conn *websocket.Conn) {
-			for {
-				messageType, msg, err := conn.ReadMessage()
-				log.Println("msg type:", strconv.Itoa(messageType))
-				if err != nil {
-					_ = conn.Close()
-					log.Println("read msg err:", err)
-					return
-				}
-				log.Println("read msg:" + string(msg))
-
-				err = conn.WriteMessage(websocket.TextMessage, []byte("请再说一遍"))
-				if err != nil {
-					log.Println("write msg err:", err)
-				}
+		for {
+			messageType, msg, err := conn.ReadMessage()
+			log.Println("msg type:", strconv.Itoa(messageType))
+			if err != nil {
+				_ = conn.Close()
+				log.Println("read msg err:", err)
+				return
 			}
-		}(conn)
+			log.Println("read msg:" + string(msg))
+			go handleMessage(string(msg), userId)
+
+			//err = conn.WriteMessage(websocket.TextMessage, []byte("请再说一遍"))
+			//if err != nil {
+			//	log.Println("write msg err:", err)
+			//}
+		}
 	})
 
 	_ = http.ListenAndServe(":8181", nil)
